46679fc5c80cc4177339aa788f55d92f6f6469a6
nottiffchan <nottiffchan@gmail.com>
2021-03-20 21:11:16 +0800

    Create json files for data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import * as Feather from 'feather-icons';
+import * as skills from '../json/skills.json';
+import * as school from '../json/school.json';
+import * as work from '../json/work.json';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,13 @@ import * as Feather from 'feather-icons';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  skills:  any  = (skills  as  any).default;
+  school:  any  = (school  as  any).default;
+  work:  any  = (work  as  any).default;
+
+  ngOnInit() {
+  }
+
   ngAfterViewInit() {
     Feather.replace();
   }
