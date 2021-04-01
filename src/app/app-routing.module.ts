e1e7373c6d8e0abd47e8c9151f5d9afb28e27061
tiff <nottiffchan@gmail.com>
2021-04-02 01:03:27 +0800

    Small styling tweaks and bug fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    scrollPositionRestoration: 'enabled',
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import * as Feather from 'feather-icons';
 
 @Component({
@@ -16,6 +16,18 @@ export class HeaderComponent implements OnInit {
 
   ngAfterViewInit() {
     Feather.replace();
+  }
+
+  opacity;
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll() {
+    var scrollTop = window.scrollY;
+    var scrollBottom = scrollTop + window.innerHeight;
+    var pageBottom = document.body.scrollHeight;
+    var diff = pageBottom - scrollBottom;
+
+    this.opacity = diff / 300;
   }
 
 }
